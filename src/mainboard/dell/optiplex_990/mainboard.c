cb196de0b00c21d10298cc3ff4bb960b465959d5
kfazz <kfazz01@gmail.com>
2019-03-18 22:20:40 -0400

    try to get gma working

--- src/mainboard/dell/optiplex_990/mainboard.c
+++ src/mainboard/dell/optiplex_990/mainboard.c
@@ -5,9 +5,9 @@
 static void mainboard_enable(struct device *dev)
 {
 	/* FIXME: fix those values*/
-	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS,
-					GMA_INT15_PANEL_FIT_DEFAULT,
-					GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
+        install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_NONE,
+                                        GMA_INT15_PANEL_FIT_DEFAULT,
+                                        GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
 }
 
 struct chip_operations mainboard_ops = {
