40234a237b8bc9fb069ef961213542ee7f8fcdc0
Ken Fazzone <kfazz01@gmail.com>
2019-03-26 22:35:21 -0400

    Pare back the superio stuff, it breaks boot

--- src/mainboard/dell/optiplex_990/acpi_tables.c
+++ src/mainboard/dell/optiplex_990/acpi_tables.c
@@ -20,14 +20,28 @@
 /* FIXME: check this function.  */
 void acpi_create_gnvs(global_nvs_t *gnvs)
 {
-	/* Disable USB ports in S3 by default */
-	gnvs->s3u0 = 0;
-	gnvs->s3u1 = 0;
 
-	/* Disable USB ports in S5 by default */
-	gnvs->s5u0 = 0;
-	gnvs->s5u1 = 0;
+        gnvs->apic = 1;
+        gnvs->mpen = 1; /* Enable Multi Processing */
+       // gnvs->pcnt = dev_count_cpu();
+
+        /* Enable USB ports in S3 */
+        gnvs->s3u0 = 1;
+        gnvs->s3u1 = 1;
+
+        gnvs->s5u0 = 1;
+        gnvs->s5u1 = 1;
+
+        /* IGD Displays */
+        gnvs->ndid = 3;
+        gnvs->did[0] = 0x80000100;
+        gnvs->did[1] = 0x80000240;
+        gnvs->did[2] = 0x80000410;
+        gnvs->did[3] = 0x80000410;
+        gnvs->did[4] = 0x00000005;
 
 	gnvs->tcrt = 100;
 	gnvs->tpsv = 90;
+
+
 }
