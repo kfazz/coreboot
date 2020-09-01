ce9314bf07fd7970de71dbbc847f2e8a10a202fd
kfazz <kfazz01@gmail.com>
2020-08-31 22:06:26 -0400

    initial autoport results

---
+++ src/mainboard/to_be_filled_by_o.e.m/to_be_filled_by_o.e.m/acpi_tables.c
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <southbridge/intel/bd82x6x/nvs.h>
+
+/* FIXME: check this function.  */
+void acpi_create_gnvs(struct global_nvs *gnvs)
+{
+	/* The lid is open by default. */
+	gnvs->lids = 1;
+
+	/* Temperature at which OS will shutdown */
+	gnvs->tcrt = 100;
+	/* Temperature at which OS will throttle CPU */
+	gnvs->tpsv = 90;
+}
